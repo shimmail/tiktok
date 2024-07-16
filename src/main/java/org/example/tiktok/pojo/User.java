886fdaf450be0e69fe24a740e7e98bc6861790b6
shimmail <shimmail@126.com>
2024-07-16 09:39:47 +0800

    7/16 9.39

---
+++ src/main/java/org/example/tiktok/pojo/User.java
@@ -0,0 +1,43 @@
+package org.example.tiktok.pojo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * 用户
+ */
+@lombok.Data
+public class User {
+    /**
+     * 用户 ID，用户唯一标识符
+     */
+    private String id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码，需要进行加密（bcrypt）
+     */
+    private String password;
+
+    /**
+     * 头像，图片链接，可以是本地也可以是云存储
+     */
+    private String avatarUrl;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createdAt;
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updatedAt;
+    /**
+     * 删除时间
+     */
+    private LocalDateTime deletedAt;
+}
